de2094242a258d51c4839c07accc8a0e759a86f0
DoseDjalmadaDunhaJunior <djalminhajr_cunha@hotmail.com>
2019-09-05 20:01:20 -0300

    inicio

---
+++ cmake-build-debug/CMakeFiles/P0201.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P0201.dir/main.cpp.o"
+  "P0201.pdb"
+  "P0201"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P0201.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*transdutor que reconhece numeros inteiros e variaveis
+alem de construir uma matriz char em que cada linha
+ devera ter a ocorrencia de cada
+ */
+int main() {
+    cout << "Hello, World!" <<endl;
+    return 0;
+}
